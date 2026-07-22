46394e41ac812479506f43e713dbc322074e5154
Gyulo94 <135434751+Gyulo94@users.noreply.github.com>
2026-07-23 01:11:45 +0900

    feat: 프리텐다드 폰트 추가, 에러페이지 추가

---
+++ src/app/error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/shared/components/ui/button";
+import { AlertTriangle } from "lucide-react";
+
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  return (
+    <div className="h-screen flex flex-col gap-y-4 items-center justify-center">
+      <AlertTriangle className="size-6 text-muted-foreground" />
+      <p className="text-sm text-muted-foreground">
+        에러: {error?.message || "서버 요청 중 문제가 발생하였습니다."}
+      </p>
+      <Button
+        variant={"secondary"}
+        size={"sm"}
+        onClick={() => (window.location.href = "/")}
+      >
+        홈으로
+      </Button>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Noto_Sans } from "next/font/google";
+import localFont from "next/font/local";
 import Providers from "./providers";
-import { cn } from "@/shared/lib/utils";
 import "../../styles/globals.css";
 
-const notoSans = Noto_Sans({ subsets: ["latin"], variable: "--font-sans" });
-
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const pretendard = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
 });
 
 export const metadata: Metadata = {
@@ -23,13 +21,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="ko"
-      suppressHydrationWarning
-      className={cn("font-sans", notoSans.variable)}
-    >
+    <html lang="ko" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50`}
+        className={`${pretendard.variable} font-pretendard antialiased bg-slate-50`}
       >
         <Providers>{children}</Providers>
       </body>
